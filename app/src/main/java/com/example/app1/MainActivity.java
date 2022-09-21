bfaa23ca36afc7ba1cbbd44712904f4f9cd44f8a
Rosmaryn11 <rosmaRyn71@gmail.com>
2022-09-21 15:47:16 +0300

    Added Change Text Button

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.app1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Method to change the TextView content
+    public void btnChangeTextClick(View v){
+        TextView tv = (TextView)findViewById(R.id.MainTextView);
+        tv.setText("New Text");
     }
 }
